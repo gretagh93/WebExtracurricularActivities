ee14542d8a3373b82873740c8cb801a9ecc5f2cc
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2024-06-07 12:38:07 +0200

    test

--- pages/1-Home.py
+++ pages/1-Home.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.markdown("# Extraescolar Bananas 🐒")
 
+st.markdown("Holiii")
+
 if st.button("Matrícula"):
     st.switch_page("pages/2-Matricula.py")
 if st.button("Horarios"):
     st.switch_page("pages/3-Horarios.py")
 
-st.markdown("Holiii")
