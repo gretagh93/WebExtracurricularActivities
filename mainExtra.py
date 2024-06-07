8d73e570d372efcc8e3fea2905120f4e39966068
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2024-06-07 12:35:23 +0200

    Buttons and navigation menu

--- mainExtra.py
+++ mainExtra.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown("# Extraescolar Bananas 🐒")
-
-if st.button('Balloons?'):
-    st.markdown("Welcome!")
+if st.button("Home"):
+    st.switch_page("pages/1-Home.py")
+if st.button("Matrícula"):
+    st.switch_page("pages/2-Matricula.py")
+if st.button("Horarios"):
+    st.switch_page("pages/3-Horarios.py")
