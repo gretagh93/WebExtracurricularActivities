f0c1234c25d22feb00fbe5a360ae537a7afa4166
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2024-06-07 12:36:48 +0200

    Navigation buttons in home page

--- pages/1-Home.py
+++ pages/1-Home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.markdown("# Extraescolar Bananas 🐒")
+
+if st.button("Matrícula"):
+    st.switch_page("pages/2-Matricula.py")
+if st.button("Horarios"):
+    st.switch_page("pages/3-Horarios.py")
